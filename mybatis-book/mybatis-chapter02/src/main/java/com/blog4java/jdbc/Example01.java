9d99cb9ab6b66823d5f71d43889f7a1f1ee66984
lhz <2190732276@qq.com>
2020-09-22 17:57:13 +0800

    :sparkles: jdbc

--- mybatis-book/mybatis-chapter02/src/main/java/com/blog4java/jdbc/Example01.java
+++ mybatis-book/mybatis-chapter02/src/main/java/com/blog4java/jdbc/Example01.java
@@ -13,12 +13,13 @@ public class Example01 {
         DbUtils.initData();
         try {
             // 加载驱动
-            Class.forName("org.hsqldb.jdbcDriver");
+           // Class.forName("org.hsqldb.jdbcDriver");
             // 获取Connection对象
             Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:mybatis",
                     "sa", "");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from user");
+             statement.execute("select * from user");
+            ResultSet resultSet = statement.getResultSet();
             // 遍历ResultSet
             ResultSetMetaData metaData = resultSet.getMetaData();
             int columCount = metaData.getColumnCount();
@@ -37,4 +38,35 @@ public class Example01 {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void myTest() {
+        // 初始化数据
+        DbUtils.initData();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:hsqldb:mem:mybatis",
+                    "sa", "");
+            PreparedStatement statement = conn.prepareStatement("select * from user");
+            statement.execute();
+            ResultSet resultSet = statement.getResultSet();
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    String val = resultSet.getString(metaData.getColumnName(i));
+                    System.out.println(columnName + " : " + val);
+                }
+                System.out.println("--------------------------------------");
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
